293d9ff6f98f5251c28402d5824b9796d067d32a
sayurrrri <emt050319@gmail.com>
2024-11-27 15:54:13 +0800

    base setting

---
+++ src/routers/index.ts
@@ -0,0 +1,14 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/home.vue')
+        }
+    ]
+})
+
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="home">
+        <h1 style="font-family:'Times New Roman', Times, serif;"> 23软件工程一班web小组作业</h1>
+    </div>
+</template>
